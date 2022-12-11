c491c15abd46cccb974186cddf01db34f2ec7974
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-11 17:29:16 +0300

    Implement password change using django authentication

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     # Define django authentication urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
+
 ]
