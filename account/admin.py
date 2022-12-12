596e38d4ba427626d979be439c204be92bf1e6ff
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-12 14:24:07 +0300

    Implement profile model to extend default user model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from account.models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    # Register profile model in admin site
+    list_display = ["user", "date_of_birth", "photo"]
+    raw_id_fields = ["user"]
