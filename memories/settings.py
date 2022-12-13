7351071b0c77d76314c4b274792933ca1a12f08e
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-13 19:49:12 +0300

    Create a profile for user registered using social media apps using django social pipeline

--- memories/settings.py
+++ memories/settings.py
@@ -159,3 +159,18 @@ AUTHENTICATION_BACKENDS = [
 # Define facebook authentication credentials
 SOCIAL_AUTH_FACEBOOK_KEY = str(os.getenv('SOCIAL_AUTH_FACEBOOK_KEY'))
 SOCIAL_AUTH_FACEBOOK_SECRET = str(os.getenv('SOCIAL_AUTH_FACEBOOK_SECRET'))
+
+# Define a pipline for login using social apps
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    # Create user profile when user chooses social media authentication
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
