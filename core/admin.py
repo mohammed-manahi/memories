4b6c8ab3fae9a585cea7a3c2d77eb38d6d75d847
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-15 02:09:51 +0300

    Create image model and register it in the admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Image
 
-# Register your models here.
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    # Register image model in admin site
+    list_display = ["title", "slug", "image", "created_at"]
+    list_filter = ["created_at"]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Image(models.Model):
+    # Create image model and associate many-to-one relation with user model for user creates images
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="images_created", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+    url = models.URLField(max_length=2000)
+    image = models.ImageField(upload_to="images/%Y/%m/%d")
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Associate many-to-many relation with user model for users like the images
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="images_liked", blank=True)
+
+    class Meta:
+        indexes = [models.Index(fields=["created_at"])]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        # Override save method to automatically assign the slug name based on the title using slugify
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
