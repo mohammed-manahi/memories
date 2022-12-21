30a9e271b6203864fdaa0085e0667784013302f3
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-21 16:38:47 +0300

    Implement generic relationship for action model to build activity stream

--- activity/admin.py
+++ activity/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from activity.models import Action
 
-# Register your models here.
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    # Register action model in admin site
+    list_display = ["user", "verb", "target", "created_at"]
+    list_filter = ["created_at"]
+    search_fields = ["verb"]
--- activity/models.py
+++ activity/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Action(models.Model):
+    # Create action model
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="actions")
+    verb = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Add content type to build generic relationship for activity stream with models of installed apps
+    target_content_type = models.ForeignKey(ContentType, blank=True, null=True, related_name="target_object",
+                                            on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey("target_content_type", "target_id")
+
+    class Meta:
+        indexes = [models.Index(fields=["-created_at"]), models.Index(fields=["target_content_type", "target_id"])]
+        ordering = ["-created_at"]
